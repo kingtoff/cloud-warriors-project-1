aef0a4c23291caba23763732ea0028e46672729d
Fortune Ogunwusi <99415191+kingtoff@users.noreply.github.com>
2023-04-25 01:45:36 +0100

    Update main.tf

--- backend-state/main.tf
+++ backend-state/main.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "warriror_terraform_state" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "team_warriors_s3_bucket_server_side_encryption" {
-  bucket = aws_s3_bucket.warriror_terraform_state.id
+  bucket = aws_s3_bucket.warriror_terraform_state.bucket
 
   rule {
     apply_server_side_encryption_by_default {
