966f17b80537bb4df9d16d14afc914c8147ed2e9
Fortune Ogunwusi <99415191+kingtoff@users.noreply.github.com>
2023-04-25 02:30:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
-
+  terraform { 
+       backend "s3" {
+      bucket  = "my-terraform-backend-statefiles-bucket"
+      key = "tf-infra/terraform.tfstate"
+      region = "us-east-1"
+      dynamodb_table = "terraform-state-locking"
+      encrypt = true
+    }
+  }
 
 # Import modules
 module "vpc" {
