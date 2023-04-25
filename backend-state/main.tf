9db759e275e3608188c297eac90ade9fe0ac221f
Fortune Ogunwusi <99415191+kingtoff@users.noreply.github.com>
2023-04-25 01:26:49 +0100

    Update main.tf

--- backend-state/main.tf
+++ backend-state/main.tf
@@ -11,3 +11,14 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "team_warriors_s3_
     }
   }
 }
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
